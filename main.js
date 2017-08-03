20593783a842bacbbb026a399dd654948a1091c5
alexgubanow <alexgubanow@ukr.net>
2017-08-03 15:31:25 +0300

    try create bootstrap element with react

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('contentSheet'));
+var AllContent = React.createClass({
+    render: function() {
+        return (
+            <div className="container-fluid">
+            <div className="row">
+                <div className="col-xs-12 col-sm-3 col-md-3" style={{'background-color' : 'lavender'}}>{this.props.text}</div>
+            </div>
+          </div>
+        );
+    }
+});
+
+ReactDOM.render(<AllContent text="saxsacs"/>, document.getElementById('contentSheet') );
