5b56e5aadff476f3c74cdf8cd1eb35e56c970105
alexgubanow <alexgubanow@ukr.net>
2017-08-04 03:42:38 +0300

    Fix css and put some margins

--- main.js
+++ main.js
@@ -89,23 +89,23 @@ class Buttons extends React.Component {
    render() {
        return (
         <div className="container-fluid">
-        <button type="button" id="EditButton" 
+        <button type="button" id="EditButton"  style={{'marginRight': '5px'}}
         className={this.state.EditButtonIsEnable ? "btn btn-default navbar-btn" :"btn btn-default navbar-btn hidden"} 
         onClick={el => this.handleClickEditButton(el)}>Edit</button>
 
-        <button type="button" id="DoneButton" 
+        <button type="button" id="DoneButton"  style={{'marginRight': '5px'}}
         className={this.state.DoneButtonIsEnable ? "btn btn-default navbar-btn" :"btn btn-default navbar-btn hidden"} 
         onClick={el => this.handleClickDoneButton(el)}>Done</button>
 
-        <button type="button" id="AddButton" 
+        <button type="button" id="AddButton"  style={{'marginRight': '5px'}}
         className={this.state.AddButtonIsEnable ? "btn btn-default navbar-btn" :"btn btn-default navbar-btn hidden"} 
         onClick={el => this.handleClickAddButton(el)}>Add</button>
 
-        <button type="button" id="RemoveAllButton" 
+        <button type="button" id="RemoveAllButton"  style={{'marginRight': '5px'}}
         className={this.state.RemoveAllButtonIsEnable ? "btn btn-default navbar-btn" :"btn btn-default navbar-btn hidden"} 
         onClick={el => this.handleClickRemoveAllButton(el)}>Remove all</button>
 
-        <button type="button" id="LoadTestButton" 
+        <button type="button" id="LoadTestButton"  style={{'marginRight': '5px'}}
         className={this.state.LoadTestButtonIsEnable ? "btn btn-default navbar-btn" :"btn btn-default navbar-btn hidden"} 
         onClick={el => this.handleClickLoadTestButton(el)}>Load test arr</button>
         </div>
@@ -134,26 +134,22 @@ class Buttons extends React.Component {
     }
    render() {
        return (
-        <div className="well col-xs-12 col-sm-3">
+        <div className="well col-xs-12 col-sm-3" style={{'marginRight': '20px'}}>
         <button type="button" className={this.props.isEdit ? "close" :"close hidden"} 
          aria-label="Close" onClick={el => this.handleRemoveItem(el)}>
          <span id={"closeBtn" + this.props.id} aria-hidden="true">&times;</span></button>
             <ul className="list-unstyled">
-                <li><img src={this.props.image} className="img-thumbnail img-rounded" alt={this.props.name}/></li>
+                <li><img src={this.props.image} className="img-thumbnail img-rounded" style={{'marginBottom': '10px'}} alt={this.props.name}/></li>
                 <li style={{"verticalAlign":"middle"}}>
                     <div className="row">
-                    <div className="col-sm-6">
-                    <span className="pull-left">{this.props.name}</span>
-                    </div>
-                    <div className="col-sm-6">
-                    <span className={this.props.isEdit ? "pull-right hidden" : "pull-right"}>{this.state.price}</span>
-                    <span className={this.props.isEdit ? "pull-right" :"pull-right hidden"}>
-                        <input type="text" 
-                        id={"priceInput" + this.props.id} 
-                        className="form-control" value={this.state.price} 
-                        onChange={el => this.handleNewPrice(el)}
-                        onClick={el => this.handleClickSavePrice(el)}/></span>
-                    </div>
+                        <div className="col-sm-6">
+                        <p className="form-control-static">{this.props.name}</p></div>
+                        <div className="col-sm-6">
+                            <p className={this.props.isEdit ? "form-control-static pull-right hidden" :"form-control-static pull-right"}>{this.state.price} EUR</p>
+                            <input type="text" id={"priceInput" + this.props.id}
+                            className={this.props.isEdit ? "form-control pull-right":"form-control pull-right hidden"} value={this.state.price} 
+                            onChange={el => this.handleNewPrice(el)} onClick={el => this.handleClickSavePrice(el)}/>
+                        </div>
                     </div>
                 </li>
             </ul>            
@@ -181,7 +177,7 @@ class GridElems extends React.Component {
     render() {
         return (
             <div className="container-fluid">
-            <div className="row">
+            <div className="row" style={{'display': 'flex','display': '-webkit-flex','flexWrap': 'wrap'}}>
                 {this.state.displayedItems.map(elem => 
                     <GridElem key={elem.id} onRemove={this.RemoveItem} id={elem.id} image={elem.image} price={elem.price} name={elem.name} isEdit={this.props.isEdit} onChange={this.handleChange}/>)}
             </div>
